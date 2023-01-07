b89532f47b0e2115b1cf85c5521f8765da9cd516
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-01-07 16:06:19 -0500

    modified package.json to include test script, modified employee.test.js

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,24 @@
-const test = require("jest");
+const Employee = require("../lib/Employee");
+
+describe("Employee class", () => {
+
+    describe("Create New Employee", () => {
+        it("should create a new employee object", () => {
+            const employee = new Employee("John", 89, "johnnyBravo@gmail.com");
+
+            expect(employee.name).toBe("John");
+            expect(employee.id).toBe(89);
+            expect(employee.email).toBe("johnnyBravo@gmail.com");
+        })
+    })
+
+    describe("getRole method", () => {
+        it("should return the role 'employee'", () => {
+            const employee = new Employee("John", 89, "johnnyBravo@gmail.com");
+            
+            expect(employee.getRole).toBe("employee");
+        });
+    });
+});
+
+
