de921fa6e3df916c680e64418eab9bd22190827b
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-01-07 16:53:42 -0500

    modified Manager.test.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,23 @@
-const test = require("jest");
+const Manager = require("../lib/Manager");
+const test = new Manager("Reginald", 100, "ReggieManages@gmail.com", 123);
+
+describe("Manager class", () => {
+
+    describe("Create New Manager", () => {
+        it("should create a new manager object", () => {
+            expect(test.officeNumber).toEqual(123);
+        })
+    });
+
+    describe("getOfficeNum method", () => {
+        it("should return the office number of the manager", () => {           
+            expect(test.getOfficeNum()).toEqual(123);
+        });
+    });
+
+    describe("getRole method", () => {
+        it("should return the role 'Manager'", () => {          
+            expect(test.getRole()).toEqual("Manager");
+        });
+    });
+});
